f4b2c804600c23d1c9727f1b47b5596a66806981
Arthur Damm <indigoarthur@gmail.com>
2019-06-26 01:47:32 +0000

    Test Laura's zombie

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,8 +1,9 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * infinite_while - loops forever
+ * infinite_while - creates an infinite loop to make the program hang
  * Return: always 0
  */
 int infinite_while(void)
@@ -15,21 +16,22 @@ int infinite_while(void)
 }
 
 /**
- * main - entry point
- * Return: 0 on success, 1 on failure.
+ * main - creates 5 zombie processes
+ * Return: always 0
  */
 int main(void)
 {
-	pid_t pid;
-	size_t i;
+	int i;
+	pid_t zombie;
 
 	for (i = 0; i < 5; i++)
 	{
-		pid  = fork();
-		if (!pid)
+		zombie = fork();
+		if (!zombie)
 			return (0);
-		printf("Zombie process created, PID: %d\n", pid);
+		printf("Zombie process created, PID: %d\n", zombie);
 	}
+
 	infinite_while();
 	return (0);
 }
