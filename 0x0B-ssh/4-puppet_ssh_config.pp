e30052859e4391de06541215c10efa8e7cdd5823
Arthur Damm <indigoarthur@gmail.com>
2019-08-06 13:44:55 +0000

    Don't match, just ensure line

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,16 +1,12 @@
 # Turns off password authentication and uses holberton key
 file_line { 'Turn off passwd auth':
-  ensure   => 'present',
-  path     => '/etc/ssh/ssh_config',
-  match    => 'PasswordAuthentication',
-  line     => '    PasswordAuthentication no',
-  multiple => true,
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  ensure   => 'present',
-  path     => '/etc/ssh/ssh_config',
-  match    => 'IdentityFile',
-  line     => '    IdentityFile ~/.ssh/holberton',
-  multiple => true,
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
 }
