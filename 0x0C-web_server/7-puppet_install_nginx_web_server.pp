933ca1cda9eb74acd797bd4a9b7d844a754638a6
Arthur Damm <indigoarthur@gmail.com>
2019-08-08 06:51:52 +0000

    add 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,9 @@
+# install nginx
+exec {'/usr/bin/env apt-get -y update': }
+exec {'/usr/bin/env apt-get -y install nginx': }
+exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
+exec {'/usr/bin/env service nginx start': }
+
