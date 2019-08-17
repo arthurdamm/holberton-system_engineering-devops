877daf97f6b91bca2543e7dca9e3d38616bdc905
Arthur Damm <indigoarthur@gmail.com>
2019-08-17 22:36:22 +0000

    add 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,26 +1,19 @@
-# install nginx
-exec { 'A':
-  command => '/usr/bin/env apt-get -y update',
-}
-exec { 'B':
-  command => '/usr/bin/env apt-get -y install nginx',
-}
-exec { 'C':
-  command => '/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html',
-}
-exec { 'D':
-  command => '/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default',
-}
-exec { 'E':
-  command => '/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default',
-}
-exec { 'F':
-  command => '/usr/bin/env sed -i "/server_name _;/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default',
-}
-exec { 'G':
-  command => '/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html',
-}
-exec { 'H':
-  command => '/usr/bin/env service nginx start',
+# Installs nginx n stuff
+exec { '/usr/bin/env apt-get -y update' :
+} ->
+package { 'nginx':
+  ensure => installed,
+} ->
+file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
+} ->
+file_line { 'add header' :
+  path   => '/etc/nginx/sites-available/default',
+  ensure => present,
+  line   => "\tadd_header X-Served-By $hostname;",
+  after  => 'server_name _;',
+} ->
+service { 'nginx':
+  ensure => running,
 }
 
