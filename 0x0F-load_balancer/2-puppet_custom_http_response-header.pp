bf28a296cd34d3fca6dd713938a36feed99f38a1
Arthur Damm <indigoarthur@gmail.com>
2019-08-17 22:39:24 +0000

    add 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,19 +1,18 @@
 # Installs nginx n stuff
-exec { '/usr/bin/env apt-get -y update' :
-} ->
-package { 'nginx':
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
   ensure => installed,
-} ->
-file { '/var/www/html/index.html' :
+}
+-> file { '/var/www/html/index.html' :
   content => 'Holberton School!',
-} ->
-file_line { 'add header' :
-  path   => '/etc/nginx/sites-available/default',
+}
+-> file_line { 'add header' :
   ensure => present,
-  line   => "\tadd_header X-Served-By $hostname;",
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
   after  => 'server_name _;',
-} ->
-service { 'nginx':
+}
+-> service { 'nginx':
   ensure => running,
 }
 
