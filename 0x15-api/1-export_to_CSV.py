9eea627f2708800c7acd8c454a16135d2238f456
Arthur Damm <indigoarthur@gmail.com>
2019-09-16 23:23:33 +0000

    add 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Reads todo list from api for id passed"""
+'''Reads todo list from api for id passed'''
 
 import csv
 import requests
@@ -12,35 +12,35 @@ def do_request():
     '''Performs request'''
 
     if not len(sys.argv):
-        return print("USAGE:", __name__, "<employee id>")
+        return print('USAGE:', __file__, '<employee id>')
     eid = sys.argv[1]
     try:
         _eid = int(sys.argv[1])
     except ValueError:
-        return print("Employee id must be an integer")
+        return print('Employee id must be an integer')
 
-    response = requests.get(base_url + "users/" + eid)
+    response = requests.get(base_url + 'users/' + eid)
     if response.status_code == 404:
-        return print("User id not found")
+        return print('User id not found')
     elif response.status_code != 200:
-        return print("Error: status_code:", response.status_code)
+        return print('Error: status_code:', response.status_code)
     user = response.json()
 
-    response = requests.get(base_url + "todos/")
+    response = requests.get(base_url + 'todos/')
     if response.status_code != 200:
-        return print("Error: status_code:", response.status_code)
+        return print('Error: status_code:', response.status_code)
     todos = response.json()
     user_todos = [todo for todo in todos
                   if todo.get('userId') == user.get('id')]
     completed = [todo for todo in user_todos if todo.get('completed')]
 
-    with open(eid + ".csv", 'w') as csvfile:
+    with open(eid + '.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, lineterminator='\n',
                             quoting=csv.QUOTE_ALL)
-        [writer.writerow(["{}".format(field) for field in
+        [writer.writerow(['{}'.format(field) for field in
                           (todo.get('userId'), user.get('username'),
                            todo.get('completed'), todo.get('title'))])
          for todo in user_todos]
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     do_request()
