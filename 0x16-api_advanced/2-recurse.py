5949cd1b8ffc64576f41ff47a839eeb64cd0bfe0
Arthur Damm <indigoarthur@gmail.com>
2019-09-19 20:54:09 +0000

    add 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Get ALL hot posts'''
+import pprint
+import requests
+
+BASE_URL = 'http://reddit.com/r/{}/hot.json'
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    '''Get ALL hot posts'''
+    headers = {'User-agent': 'Unix:0-subs:v1'}
+    params = {'limit': 100}
+    if isinstance(after, str):
+        if after != "STOP":
+            params['after'] = after
+        else:
+            return hot_list
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers, params=params)
+    if response.status_code != 200:
+        return hot_list
+    data = response.json().get('data', {})
+    after = data.get('after', 'STOP')
+    if not after:
+        after = "STOP"
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+    return recurse(subreddit, hot_list, after)
