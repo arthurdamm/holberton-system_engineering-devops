6fc2092e6d7a4e7f04b8bec3b5896497bfca9a8e
Arthur Damm <indigoarthur@gmail.com>
2019-09-19 21:53:47 +0000

    add 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Get ALL hot posts'''
 import pprint
+import re
 import requests
 
 BASE_URL = 'http://reddit.com/r/{}/hot.json'
@@ -28,6 +29,7 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
                            for post in data.get('children', [])]
     return count_words(subreddit, word_list, hot_list, after)
 
+
 def print_results(word_list, hot_list):
     '''Prints request results'''
     count = {}
@@ -35,7 +37,8 @@ def print_results(word_list, hot_list):
         count[word] = 0
     for title in hot_list:
         for word in word_list:
-            count[word] = count[word] + title.count(word + ' ')
+            count[word] = count[word] +\
+             len(re.findall('\s{}\s'.format(word), title, re.I))
 
     count = {k: v for k, v in count.items() if v > 0}
     for word in sorted(list(count.keys()),
