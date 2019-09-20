49887eeb946da193c660f4eae24140a24022ae33
Arthur Damm <indigoarthur@gmail.com>
2019-09-20 01:03:49 +0000

    add 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -38,7 +38,7 @@ def print_results(word_list, hot_list):
     for title in hot_list:
         for word in word_list:
             count[word] = count[word] +\
-             len(re.findall(r'\b{}(?:\b|[^.!_])'.format(word), title, re.I))
+             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
 
     count = {k: v for k, v in count.items() if v > 0}
     words = sorted(list(count.keys()))
